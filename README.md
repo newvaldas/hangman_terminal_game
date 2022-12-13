3481787a31def465356e9c1addcc9d4e14e2c6a5
newvaldas <vrldas11@gmail.com>
2022-12-13 13:20:40 +0200

    Add game files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction to Hangman game
+
+The Hangman is a classic game where a player needs to guess a word where all the letters are masked. The player has a certain number of attempts and needs to select a letter: if the selected letter is part of the word all the locations with the selected letter are unmasked, on the other hand if the selected letter is not contained in the word then the number of attempts of the player decreases by one. The player wins if finds the word within the number of attempts and loses otherwise.
+
+# Installation
---
+++ main.py
@@ -0,0 +1,107 @@
+import numpy as np
+import string
+
+def load_file(file_path) -> list:
+        random_words=[]
+        f = open(file_path, "r")
+        for x in f:
+            random_words.append(x.strip("\n"))
+        f.close()
+        return random_words
+
+
+def mask_word(word) -> str:
+        masked_word=''
+        for character in word:
+            if(character!=' '):
+                masked_word=masked_word+'*'
+            else:
+                masked_word=masked_word+' '
+        return masked_word  
+
+def select_letter(letters) -> list:
+        index=-1
+        while(index==-1):
+            l=input("Choose Letter: ")
+            try:
+                index=letters.index(l)
+            except:
+                print("Letter not in the list! Try again!")
+        return [index,l]
+
+def update_mask(masked_word,word,letter) -> list:
+        found=0
+        new_masked_word=''
+        for i in range(len(word)):
+            if(letter==word[i]):
+                new_masked_word+=letter
+                found=1
+            else:
+                new_masked_word+=masked_word[i]
+        if(found==1):
+            print("The selected letter is part of the word!")
+        else:
+            print("Wrong letter!")
+        return [new_masked_word,found]
+
+def reduce_health(health,found) -> int:
+        if(found==0):
+            health-=1
+        return health
+
+def print_letters(letters) -> str:
+        print("Available Letters: ")
+        s='|'
+        for l in letters:
+            s=s+l+"|"
+        print(s)
+        
+def print_info(masked_word,health) -> str:
+        print("Health: "+"❤"*health)
+        print("Word: "+masked_word)
+        
+def check_game_over(masked_word,health):
+        game_over=0
+        if(health==0 or masked_word.find("*")==-1):
+            game_over=1
+        return game_over 
+
+# -------------------------main game loop----------------------------------
+
+file_path="dictionary.txt"  
+random_words=load_file(file_path)
+letters=list(string.ascii_lowercase)
+index=np.random.randint(len(random_words))
+word=random_words[index].lower()
+masked_word=mask_word(word)
+health=10
+game_over=0
+while(not game_over):
+    print ("========Welcome to the Hangman game!============")
+    print ("===============Good luck!=======================\n")
+
+   
+    print_info(masked_word,health)
+    
+    print_letters(letters)
+  
+    [i,l]=select_letter(letters)
+    
+    letters.pop(i)
+   
+    [masked_word,found]=update_mask(masked_word,word,l)
+  
+    health=reduce_health(health,found)
+    
+    game_over=check_game_over(masked_word,health)
+    print("----------")
+
+if(health>0):
+    print("You Won!")
+else:
+    print("You Lost!")
+print("The word was: ",(word.upper())) 
+
+
+
+    
